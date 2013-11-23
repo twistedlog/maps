faa48b2ed7ebfb4d21169b744d0432000a1a0d66
Ubuntu <ubuntu@ip-172-31-13-222.us-west-2.compute.internal>
2013-11-23 00:29:01 +0000

    added models

--- gmaps/gmaps/apps/googlemaps/models.py
+++ gmaps/gmaps/apps/googlemaps/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Cities(models.Model):
+    """ Master Table for cities with (long, lat)"""
+    city = models.CharField(max_length=200)
+    longitude = models.IntField()
+    latitude = models.IntField()
+
+
+class Path(models.Model):
+    """Path model contains master list of Paths"""
+    src = models.ForeignKey(Cities)
+    dest = models.ForeignKey(Cities)
+
+
+class Routes(models.Model):
+    """Contains all the hops in bw a path"""
+    src = models.ForeignKey(Cities)
+    dest = models.ForeignKey(Cities)
+    path = models.ForeignKey(Path)
+    priority = models.IntField()
