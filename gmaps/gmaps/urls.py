92ebe467a1e4b0d9b0c85f37b634f6f2280c4a8e
twistedlog <sahil2422@gmail.com>
2013-11-26 01:45:57 +0530

    ichanged view name

--- gmaps/gmaps/urls.py
+++ gmaps/gmaps/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
-from apps.googlemaps.views import RoutesView, RouteView
+from apps.googlemaps.views import PathView, RouteView
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('',
@@ -11,9 +11,9 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', login_required(RoutesView.as_view())),
-    url(r'^paths/$', login_required(RoutesView.as_view())),
-    url(r'path/(?P<id>\d)/$', RoutesView.as_view()),
+    url(r'^$', login_required(PathView.as_view())),
+    url(r'^paths/$', login_required(PathView.as_view())),
+    url(r'path/(?P<id>\d)/$', RouteView.as_view()),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
 )
