1ce739474d79d87d7996d50afa0874c06e0f866d
twistedlog <sahil2422@gmail.com>
2013-11-26 02:19:59 +0530

    half baked view

--- gmaps/gmaps/apps/googlemaps/views.py
+++ gmaps/gmaps/apps/googlemaps/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.views.generic.base import View
-from .models import Path
+from .models import Path, Routes, RouteDetail
+from django.http import HttpResponse
+import json
 
 # Create your views here.
 
-class RoutesView(View):
+
+class PathView(View):
+
+    """PathView."""
+
     http_method_names = ['get']
 
     def get(self, request):
@@ -13,7 +19,26 @@ class RoutesView(View):
 
 
 class RouteView(View):
+
+    """RouteView."""
+
     http_method_names = ['post']
 
     def post(self, request, id):
-        pass
+        config = json.loads(request.body)
+        if not config['config']['next']:
+            # first time
+            path = Path.objects.get(id=id)
+            length = len(Routes.objects.filter(path=path))
+            route = Routes.objects.filter(path=path)[0]
+            next = route.id
+            marker = [route.src.longitude, route.dest.latitude]
+            coordinates = None
+
+            return_config = {
+                'length': length,
+                'next': next,
+                'marker': marker,
+                'coordinates': coordinates,
+            }
+            return HttpResponse(json.dumps(return_config), content_type="application/json")
